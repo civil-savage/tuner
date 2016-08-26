07ffa020a4442f67dbb4bc44dc4f74309f65ec1b
rhohman <rhohman@workbox.workbox>
2016-08-26 07:45:20 -0400

    added notes

--- ft.py
+++ ft.py
@@ -1,9 +1,19 @@
+import csv
+import os
 from flask import Flask, render_template, request, redirect, url_for, escape
 from pyo import *
-import os
+
 
 app = Flask(__name__)
 app.debug = True
+
+with open('notes.csv', 'rb') as notefile:
+    csv = csv.reader(notefile)
+    notes = []
+    for row in csv:
+        print row
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
