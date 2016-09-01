7e63c6db743af95a39f54bcb6d599e4c1fa95c43
Robert Hohman <rhohman@rhohman.odonnellpdc.com>
2016-09-01 15:42:29 -0400

    rename x.html to tuner.html

--- tuner_app.py
+++ tuner_app.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 from flask import Flask, render_template, request, redirect, url_for, escape
 from pyo import *
 
@@ -8,14 +9,23 @@ app = Flask(__name__)
 app.debug = True
 
 
-def ser():
-    s = Server().boot()
-    path = os.path.join(os.path.expanduser("~/tuner/tuner/static"), "synth.wav")
-    s.recordOptions(dur=10, filename=path, fileformat=0, sampletype=1)
-    a = Sine(500,.2)
-    s.recstart()
+# def ser():
+#     s = Server(audio="offline_nb").boot()
+#     path = os.path.join(os.path.expanduser("~/xdev/static"), "synth.ogg")
+#     s.recordOptions(dur=5, filename=path, fileformat=7, sampletype=1)
+#     s.start()
+#     # Creates an amplitude envelope
+#     amp = Fader(fadein=1, fadeout=1, dur=10, mul=0.3).play()
+
+#     # A Simple synth
+#     lfo = Sine(freq=[0.15, 0.16]).range(1.25, 1.5)
+#     fm2 = CrossFM(carrier=200, ratio=lfo, ind1=10, ind2=2, mul=amp).out()
+#     time.sleep(5)
 
 
+
+    
+# ser()
 with open('notes.csv', 'rb') as notefile:
     csv = csv.reader(notefile)
     notes = []
@@ -25,8 +35,7 @@ with open('notes.csv', 'rb') as notefile:
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    ser()
-    return render_template("x.html", notes=notes)
+    return render_template("tuner.html", notes=notes)
 
 
 # IFGUARD #
