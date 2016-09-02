2fabd21f3a6a4952322f4c018cbfc8979604432c
rhohman <rhohman@workbox.workbox>
2016-09-02 06:43:40 -0400

    delete junk

--- tuner_app.py
+++ tuner_app.py
@@ -8,12 +8,6 @@ app = Flask(__name__)
 app.debug = True
 
 
-def ser():
-    s = Server().boot()
-    path = os.path.join(os.path.expanduser("~/tuner/tuner/static"), "synth.wav")
-    s.recordOptions(dur=10, filename=path, fileformat=0, sampletype=1)
-    a = Sine(500,.2)
-    s.recstart()
 
 
 with open('notes.csv', 'rb') as notefile:
@@ -25,7 +19,6 @@ with open('notes.csv', 'rb') as notefile:
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    ser()
     return render_template("x.html", notes=notes)
 
 
